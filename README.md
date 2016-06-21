0823b1a078ca865b876e3b7ae04c6df6bab124d5
Victor Alencar <victor.a.alencar@gmail.com>
2016-06-21 15:26:29 -0300

    Docs

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-# Tax Easy
+Tax Easy
+=====
 
 [![Code Climate](https://codeclimate.com/github/valencar/tax_easy/badges/gpa.svg)](https://codeclimate.com/github/valencar/tax_easy)  [![Travis](https://travis-ci.org/valencar/tax_easy.svg?branch=master)](https://travis-ci.org/valencar/tax_easy)
 
-Gem to calculate Taxes
+Ruby Gem to calculate Taxes.
 
-First Addition: Brazil
+----------
+
+### Installation
+
+```ruby
+gem 'tax_easy', github: 'valencar/tax_easy'
+```
+
+Run the bundle command to install it.
+
+----------
+
+### Calculators - Brazil
+
+
+#### IOF
+
+```ruby
+TaxEasy::Financial::Brazil::IOF.new.calculate(20_000, 48, 1.5) # ==> 356.70671942746617
+```
+
+IOF varies according to type of loan. We need to implement them.
+
+You can override the default base IOF value. Example:
+
+```ruby
+TaxEasy::Financial::Brazil::IOF.new(iof_day: 0.0038, iof_additional: 0.000050)
+```
+
+### TODO
+
+There's a lot of taxes to be implemented.
+
+So feel free to contribute! \o/.
+
+
+## Contributing
+ 1. Fork the repository
+ 2. Create a branch
+ 4. Create a spec + code
+ 5. Open a Pull Request ;)
--- lib/tax_easy/financial/brazil/iof.rb
+++ lib/tax_easy/financial/brazil/iof.rb
@@ -13,6 +13,16 @@ module TaxEasy
           @installment_calculator = overrides.fetch(:installment_calculator) { TaxEasy::Calculator::Installment.new }
         end
 
+        # Calculates IOF for Loan in Brazil
+        # @param loan_value [Float]
+        # @param financing_time_months [Integer] The number of payments to be made
+        # @param interest_rate [Float] The interest rate as decimal (not per cent) per period
+        # @param loan_date [Date] The loan request date
+        # @return [Float]
+        #
+        # @example
+        #   TaxEasy::Financial::Brazil::IOF.new.calculate(20_000, 48, 1.5) # ==> 356.70671942746617
+        #
         def calculate(loan_value, financing_time_months, interest_rate, loan_date = Date.today)
           balance = loan_value
           installment = installment_value(interest_rate, financing_time_months, loan_value)
@@ -36,22 +46,27 @@ module TaxEasy
 
         private
 
+        ## Calcualtes installment value for loan
         def installment_value(interest_rate, financing_time_months, loan_value)
           @installment_calculator.calculate(interest_rate, financing_time_months, loan_value)
         end
 
+        ## Calculates the final IOF base on IOF not loan value
         def loan_iof(iof, loan_value)
           (iof*loan_value)/(loan_value-iof)
         end
 
+        ## Interest part to be payed on specific installment
         def interest_part(balance, interest_rate)
           balance*(interest_rate/100)
         end
 
+        ## Past days from specific date
         def past_days(start_date, end_date)
           (end_date - start_date).to_i
         end
 
+        ## Minimal days from 365 to calculate IOF
         def minimal_days(past_days)
           [past_days, 365].min
         end
